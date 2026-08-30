5031e5b19f0a813434c53d0ce465441cce1a0163
Anderboi <Upolovnew@gmail.com>
2026-08-30 17:45:28 +0300

    feat: collapsible component

--- components/materials/materials-client.tsx
+++ components/materials/materials-client.tsx
@@ -110,7 +110,7 @@ export function MaterialsClient({
 
   return (
     <article>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {optimisticMaterials.map((mat) => (
           <MaterialCard
             key={mat.id}
